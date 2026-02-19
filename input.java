64947feccfec1ccc6f8f1ed6e0018153a33326f4
Monu <rockmonu45@gmail.com>
2026-02-19 15:37:58 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,25 @@
+// import java.util.Scanner;
+
+// public class input {
+//     public static void main(String[] args){
+//     Scanner sc = new Scanner(System.in);
+
+//     System.out.println("enter your name" );
+//     String name = sc.nextLine();
+    
+//     System.out.println("enter your age " );
+//     int age = sc.nextInt();
+
+//     System.out.println("enter your height ");
+//     double height = sc.nextDouble();
+
+//     System.out.println("you are student true or false ");
+//     boolean isStudent = sc.nextBoolean();
+
+//     System.out.println(name);
+//     System.out.println(age);
+//     System.out.println(height);
+//     System.out.println(isStudent);
+
+//     }
+// }
---
+++ variableanddatatypes.java
@@ -0,0 +1,24 @@
+// public class variableanddatatypes {
+//     public static void main(String[] args){
+
+//     //     String name = "Monu";
+//     //     int age = 19;
+//     //     char grade = 'A';
+//     //     double percentage = 85.8;
+
+
+//     // -----this code print on seperates lines--   
+//     // //     System.out.println("Name: " + name);
+//     // //     System.out.println("Age: " + age);
+//     // //     System.out.println("Grade: " + grade);
+//     // //     System.out.println("Percentage: " + percentage);
+
+
+//     // ---this is print all thing in one line---
+//     // System.out.println("My name is " + name + 
+//     //                ", I am " + age + 
+//     //                " years old, got grade " + grade + 
+//     //                " and scored " + percentage + "%.");
+
+//     }
+// }
